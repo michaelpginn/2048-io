2b3292c8948a5b922232eea73cfefe3ade147136
michaelpginn <milespergallon271@gmail.com>
2021-10-23 21:39:36 -0500

    Refactor leveling constants

--- Assets/Scripts/Model/PlayerLevel.cs
+++ Assets/Scripts/Model/PlayerLevel.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 public enum PlayerLevel
 {
     Level2,
@@ -12,4 +15,49 @@ public enum PlayerLevel
     Level512,
     Level1024,
     Level2048
+}
+
+public static class PlayerLevelMethods
+{
+    private static Dictionary<PlayerLevel, Color32> colors = new Dictionary<PlayerLevel, Color32>
+    {
+        { PlayerLevel.Level2,       new Color32(236, 77, 134, 255) },
+        { PlayerLevel.Level4,       new Color32(94, 221, 93, 255) },
+        { PlayerLevel.Level8,       new Color32(71, 138, 255, 255) },
+        { PlayerLevel.Level16,      new Color32(230, 116, 81, 255) },
+        { PlayerLevel.Level32,      new Color32(255, 248, 0, 255) },
+        { PlayerLevel.Level64,      new Color32(255, 141, 230, 255) },
+        { PlayerLevel.Level128,     new Color32(145, 57, 255, 255) },
+        { PlayerLevel.Level256,     new Color32(255, 108, 54, 255) },
+        { PlayerLevel.Level512,     new Color32(0, 159, 79, 255) },
+        { PlayerLevel.Level1024,    new Color32(83, 255, 231, 255) },
+        { PlayerLevel.Level2048,    new Color32(217, 0, 80, 255) },
+        //Tuple.Create((byte)255, (byte)0, (byte)80)
+    };
+
+    public static Color32 GetColor(this PlayerLevel level)
+    {
+        return colors[level];
+    }
+
+    private static Dictionary<PlayerLevel, float> scales = new Dictionary<PlayerLevel, float>
+    {
+        { PlayerLevel.Level2,       1.0f },
+        { PlayerLevel.Level4,       1.25f },
+        { PlayerLevel.Level8,       1.5f },
+        { PlayerLevel.Level16,      1.75f },
+        { PlayerLevel.Level32,      2.0f },
+        { PlayerLevel.Level64,      2.25f },
+        { PlayerLevel.Level128,     2.5f },
+        { PlayerLevel.Level256,     2.75f },
+        { PlayerLevel.Level512,     3.0f },
+        { PlayerLevel.Level1024,    3.25f },
+        { PlayerLevel.Level2048,    3.5f },
+    };
+
+    public static Vector3 GetScale(this PlayerLevel level)
+    {
+        var scale = scales[level];
+        return new Vector3(scale, scale, scale);
+    }
 }
--- Assets/Scripts/Model/PlayerModel.cs
+++ Assets/Scripts/Model/PlayerModel.cs
@@ -6,55 +6,22 @@ using Random = UnityEngine.Random;
 
 public class PlayerModel : MonoBehaviour
 {
-    // Array of tuples representing material colors for the different levels
-    private Tuple<byte, byte, byte>[] materialColors = new Tuple<byte, byte, byte>[]
-    {
-        Tuple.Create((byte)236, (byte)77, (byte)134),
-        Tuple.Create((byte)94, (byte)221, (byte)93),
-        Tuple.Create((byte)71, (byte)138, (byte)255),
-        Tuple.Create((byte)230, (byte)116, (byte)81),
-        Tuple.Create((byte)255, (byte)248, (byte)0),
-        Tuple.Create((byte)255, (byte)141, (byte)230),
-        Tuple.Create((byte)145, (byte)57, (byte)255),
-        Tuple.Create((byte)255, (byte)108, (byte)54),
-        Tuple.Create((byte)0, (byte)159, (byte)79),
-        Tuple.Create((byte)83, (byte)255, (byte)231),
-        Tuple.Create((byte)217, (byte)177, (byte)58),
-        Tuple.Create((byte)255, (byte)0, (byte)80)
-    };
-    
-    private Vector3[] scales = new Vector3[] {
-        new Vector3(1.0f,1.0f,1.0f),
-        new Vector3(1.25f,1.25f,1.25f),
-        new Vector3(1.5f,1.5f,1.5f),
-        new Vector3(1.75f,1.75f,1.75f),
-        new Vector3(2.0f,2.0f,2.0f),
-        new Vector3(2.25f,2.25f,2.25f),
-        new Vector3(2.5f,2.5f,2.5f),
-        new Vector3(2.75f,2.75f,2.75f),
-        new Vector3(3.0f,3.0f,3.0f),
-        new Vector3(3.25f,3.25f,3.25f),
-        new Vector3(3.5f,3.5f,3.5f),
-        new Vector3(3.75f,3.75f,3.75f)
-    };
-    
-    
     public PlayerType playerType;
     public PlayerLevel level;
 
     // Material of the player
-    private MeshRenderer meshrenderer;
+    private MeshRenderer meshRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        meshrenderer = GetComponent<MeshRenderer>();
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        // Humans start with level 2, CPUs can start at anything (can they?)
         if (playerType == PlayerType.human)
         {
             level = PlayerLevel.Level2;
         }
-            
         else
         {
             level = (PlayerLevel)Random.Range(0, (int)PlayerLevel.Level64);
@@ -64,35 +31,23 @@ public class PlayerModel : MonoBehaviour
         SetScale();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     // Changes the color of the material when player created / levels up 
-    void SetMaterial()
+    private void SetMaterial()
     {
-        (byte red, byte green, byte blue) = materialColors[(int) level];
-        Color32 color = new Color32(red, green, blue, (byte)255);
-        meshrenderer.material.color = color;
+        meshRenderer.material.color = level.GetColor();
     }
 
-    void SetScale() {
-        Vector3 scale = scales[(int) level];
-        transform.localScale = scale;
+    private void SetScale() {
+        transform.localScale = level.GetScale();
     }
-    
-    // public void SetLevel(PlayerLevel newLevel)
-    // {
-    //     level = newLevel;
-    //     SetMaterial();
-    // }
 
+    /// <summary> Levels up a player to the next level, if possible. </summary>
+    /// <returns> <b>true</b> if the player has been leveled up, <b>false</b> if they are max level. </returns>
     public bool LevelUp()
     {
         // if not at the highest level, can level-up
-        if (playerType == PlayerType.human && level < PlayerLevel.Level2048)
+        if (level < PlayerLevel.Level2048)
         {
             level += 1;
             print("Leveled up to " + level);
@@ -103,6 +58,5 @@ public class PlayerModel : MonoBehaviour
 
         // at highest level -- cannot level-up
         return false;
-
     }
 }
